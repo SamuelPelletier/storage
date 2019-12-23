b5fb36762ab904a77e9578f798de5bba23d35062
PELLETIER Samuel <pellesam.info@gmail.com>
2019-12-23 14:00:35 +0100

    Securization for delete

--- storage/delete-process.php
+++ storage/delete-process.php
@@ -5,14 +5,20 @@ if (isset($_POST['url']) && isset($_POST['index'])) {
     $data = json_decode($jsonString, true);
 
     if ($data['images'][$_POST['index']]['url'] == $_POST['url']) {
-        $data['images'][$_POST['index']]['tags'] = ['deleted'];
+        if ($_SESSION['user'] === $data['images'][$_POST['index']]['owner']) {
+            $data['images'][$_POST['index']]['tags'] = ['deleted'];
+        }
     } else {
         for ($i = 0; $i < count($data['images']); $i++) {
-            if ($data['images'][$i]['url'] === $_POST['url']) {
-                $data['images'][$i]['tags'] = ['deleted'];
+            if ($_SESSION['user'] === $data['images'][$i]['owner']) {
+                if ($data['images'][$i]['url'] === $_POST['url']) {
+                    $data['images'][$i]['tags'] = ['deleted'];
+                }
             }
         }
     }
+
+
     $newJsonString = json_encode($data);
     file_put_contents($pathConfig, $newJsonString);
 }
--- storage/js/gallery.js
+++ storage/js/gallery.js
@@ -37,8 +37,9 @@ function fillGallery() {
             var elem = data.images[numberOfImages - i - 1];
             if ((elem.year === year || year == null) && (elem.owner === owner || owner == null) && !elem.tags.includes("deleted")) {
                 counter++;
+                var deleteButton = "<div class='remove-button'><span  class='fa fa-trash' onclick='remove(\"" + elem.url + "\"," + i + ")'></span></div>";
                 $('.imageGallery1').append("<div id='image" + i + "' class='col-lg-3 col-md-4 col-sm-6 " + elem.tags.join(" ") + "'>" +
-                    "                <div class='remove-button'><span  class='fa fa-trash' onclick='remove(\"" + elem.url + "\"," + i + ")'></span></div>" +
+                    deleteButton +
                     "                <div class='h_gallery_item'>" +
                     "                    <img src='" + elem.url + "' alt=''>" +
                     "                    <div class='hover'>" +
